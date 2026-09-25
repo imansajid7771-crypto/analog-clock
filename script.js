4a40b4c0d0ce51495c97b04de926f867f3a50fd5
iman <imansajid7771@gmail.com>
2026-09-25 19:04:13 +0500

    weather

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function updateClock() {
   let minutes = now.getMinutes();
   let seconds = now.getSeconds();
   let digital_time = document.querySelector(".digtime");
-  digital_time.innerText = `${String(hours).padStart(2,"0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  digital_time.innerText = `🟢${String(hours).padStart(2,"0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
 // Update the clock hands
   hour.style.transform = `rotate(${(hours % 12) * 30 + minutes * 0.5}deg)`;
   minute.style.transform = `rotate(${minutes * 6 + seconds * 0.1}deg)`;
@@ -67,6 +67,7 @@ day2.innerText = `${days[day1.getDay()]},${currmounth}${day} `;
 // weather api
 let tem = document.querySelector(".temperature");
 let hum = document.querySelector(".humidity");
+
 const url =
   "https://api.open-meteo.com/v1/forecast?latitude=31.4187&longitude=73.0791&current=temperature_2m,relative_humidity_2m,weather_code";
 fetch(url)
@@ -74,8 +75,40 @@ fetch(url)
   .then(data => {
     console.log(data);
     let temp = data.current.temperature_2m;
-    tem.innerText = `temperature:🌤️${temp}°C`;
+    let code = data.current.weather_code;
+    document.querySelector(".weathertype").innerText =getWeather(code);
+    tem.innerText = `${temp}°C`;
     let humi = data.current.relative_humidity_2m;
     hum.innerText = `humidity: 💧 ${humi}%`;
-    console.log("Weather code:", data.current.weather_code);
+    
   })
+  function getWeather(code) {
+
+    if (code === 0) {
+        return "☀️ Clear sky";
+    }
+    else if (code >= 1 && code <= 3) {
+        return "🌤️ Partly cloudy";
+    }
+    else if (code >= 45 && code <= 48) {
+        return "🌫️ Foggy";
+    }
+    else if (code >= 51 && code <= 57) {
+        return "🌦️ Drizzle";
+    }
+    else if (code >= 61 && code <= 67) {
+        return "🌧️ Rain";
+    }
+    else if (code >= 71 && code <= 77) {
+        return "❄️ Snow";
+    }
+    else if (code >= 80 && code <= 82) {
+        return "🌧️ Rain showers";
+    }
+    else if (code >= 95 && code <= 99) {
+        return "⛈️ Thunderstorm";
+    }
+    else {
+        return "🌍 Unknown";
+    }
+}
